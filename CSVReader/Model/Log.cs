8ae064eaec40a93301fb326ba0906ee1b31a5f86
nickmax85 <nickmax85@gmx.at>
2021-12-02 22:11:44 +0100

    ....

--- CSVReader/Model/Log.cs
+++ CSVReader/Model/Log.cs
@@ -6,17 +6,18 @@ namespace CSVReader.Model
 {
     class Log
     {
+        public string Timestamp { get; set; }
+        public string Te_Amb { get; set; }
         public string Te_WE_VL1 { get; set; }
         public string Te_WE_RL { get; set; }
-        public string Te_Amb { get; set; }
         public string Te_HK1_Ra { get; set; }
         public string Te_HK1_VL { get; set; }
         public string WMZ_actn { get; set; }
         public string Te_KO_SG { get; set; }
         public string Te_KO_HG { get; set; }
         public string Te_KO_VD_p { get; set; }
+        public string Te_KO_Ko { get; set; }
         public string SM_OefWert { get; set; }
-        public string Timestamp { get; set; }
 
     }
 }
